083d97faf32290780dd585b63772a247fd0df209
Andrid S. <146693857+gameSenz@users.noreply.github.com>
2025-12-26 02:32:37 -0500

    commit 2

--- LuxBotFlask/app.py
+++ LuxBotFlask/app.py
@@ -1,16 +1,17 @@
 import os
-
+import requests
 import stripe
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, abort
 from dotenv import load_dotenv
 
 load_dotenv()
 
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+stripe_webhook_key = os.getenv('STRIPE_WEBHOOK_SECRET')
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
         price_id = 'price_1SiSKw1JlFuoKmRQmzTACToY'
@@ -49,5 +50,26 @@ def success():
 def cancel():
     return 'Payment Cancelled'
 
+@app.route('/stripe-webhook', methods=['POST'])
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, stripe_webhook_key
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('Error parsing payload: {}'.format(str(e)))
+        return abort(400, description="Invalid Payload")
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('Error verifying webhook signature: {}'.format(str(e)))
+        return abort(400, description="Invalid Signature")
+
+    if event['type'] == 'checkout.session.completed':
+#         send token
 if __name__ == '__main__':
     app.run()
