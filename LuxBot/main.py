550d32aeacc74e3737b8147f2bd965c9caa68dfb
Andrid S. <146693857+gameSenz@users.noreply.github.com>
2026-01-05 14:22:42 -0500

    procfile
    added procfile

--- LuxBot/main.py
+++ LuxBot/main.py
@@ -13,7 +13,7 @@ token = os.getenv('DISCORD_TOKEN')
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True 
+intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
---
+++ Procfile
@@ -0,0 +1,2 @@
+bot: python ./LuxBot/main.py
+backend: python ./LuxBotFlask/app.py
